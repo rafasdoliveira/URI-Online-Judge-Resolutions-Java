95cbe9515ebb4a6c084e6242b40cc109da82b077
Rafael Oliveira <101873579+rafasdoliveira@users.noreply.github.com>
2022-11-24 01:31:01 -0300

    Create URI 1000.java

---
+++ URI 1000.java
@@ -0,0 +1,18 @@
+/**Hello World!*/
+
+import java.io.IOException;
+ 
+/**
+ * IMPORTANTE: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+      /** gera uma saída de texto entre aspas duplas significando uma String, 
+      criando uma nova linha e posicionando o cursor na linha abaixo, o que é identificado pela terminação “ln” */
+        System.out.println("Hello World!");
+
+    }
+ 
+}
